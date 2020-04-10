3153b04d81503aadb1a76ff6e599685e47b4b836
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2020-04-10 16:51:36 +0200

    Adding `http` module to the project:
    * Adding relevant http status codes as constants (from `djangorestframework` project)
    * Adding HTTP methods (more details in: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods)
    * Adding custom HTTP exceptions, with custom status code and message, in order to return json response automatically in case of unhandled error.

---
+++ django_rest/http/exceptions.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+from django_rest.http import status
+
+
+class BaseAPIException(Exception):
+    """
+    Base API exception from which all specific exceptions will inherit.
+    The exception defines a `STATUS_CODE` attribute and a `RESPONSE_MESSAGE`
+    Those attributes are used by `api_view()` decorator, to return a readable
+    `JsonResponse()` with a valid message and status code, without having to catch
+    exceptions inside the view.
+    """
+
+    STATUS_CODE = status.HTTP_500_INTERNAL_SERVER_ERROR
+    RESPONSE_MESSAGE = "An unknown server error occured."
+
+
+class BadRequest(BaseAPIException):
+    STATUS_CODE = status.HTTP_400_BAD_REQUEST
+    RESPONSE_MESSAGE = "Bad request."
+
+
+class NotAuthenticated(BaseAPIException):
+    STATUS_CODE = status.HTTP_401_UNAUTHORIZED
+    RESPONSE_MESSAGE = "Unauthorized operation. Maybe forgot the authentication step ?"
+
+
+class PermissionDenied(BaseAPIException):
+    STATUS_CODE = status.HTTP_403_FORBIDDEN
+    RESPONSE_MESSAGE = "Forbidden operation. Make sure you have the right permissions."
+
+
+class NotFound(BaseAPIException):
+    STATUS_CODE = status.HTTP_404_NOT_FOUND
+    RESPONSE_MESSAGE = "The requested resource is not found."
+
+
+class MethodNotAllowed(BaseAPIException):
+    STATUS_CODE = status.HTTP_405_METHOD_NOT_ALLOWED
+    RESPONSE_MESSAGE = "HTTP Method not allowed."
+
+
+class UnknownServerError(BaseAPIException):
+    pass
+
+
+class ServiceUnavailable(BaseAPIException):
+    STATUS_CODE = status.HTTP_503_SERVICE_UNAVAILABLE
+    RESPONSE_MESSAGE = "The requested service is unavailable."
