ac80366a95b89abbe606ef6b046ba42f3d9c8973
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2020-04-17 23:27:05 +0200

    Improving the library's core decorator `@api_view`, by:
    * Supporting `deserializer_class` parameter.
    * Supporting deserializer class mapping (custom deserializers for each http method).
    * Adding `allow_forms` parameter to accept forms data or raise error.
    * Supporting for `@api_view` syntax (only `@api_view()` was supported before)
    * Checking view's type to make sure it's either a function or a `django.views.View` class
    * Normalizing request's payload between content_types, so that the output's always in the same format.
    * Extracting query params and passing custom parameters for the view (query params, payload, url params, etc.).
    * Decorating only class view's relevant methods (http methods) instead of all methods.
    * Moving helpers function into separate file.
    * Type hinting to improve code's readability.
    * Assuring support for all python's versions (2.7+, 3+), and django's versions (1.11+).

---
+++ django_rest/decorators/__init__.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+import inspect
+
+
+from django.views import View
+
+from django_rest.deserializers import (
+    Deserializer,
+    PerforatedDeserializer,
+)
+from django_rest.http.methods import ALL_HTTP_METHODS
+from django_rest.decorators.utils import (
+    build_class_wrapper,
+    build_deserializer_map,
+    build_function_wrapper,
+)
+from django_rest.permissions import AllowAny, BasePermission
+
+
+FORMS_CONTENT_TYPES = (
+    "application/x-www-form-urlencoded",
+    "multipart/form-data",
+)
+
+
+def api_view(
+    permission_class=AllowAny,  # type: Union[AbstractPermission, Any]
+    allowed_methods=ALL_HTTP_METHODS,  # type: Tuple[str]
+    deserializer_class=PerforatedDeserializer,  # type: Union[ClassVar[Deserializer], Dict[str, ClassVar[Deserializer]]]
+    allow_forms=False,  # type: bool
+):  # type:(...) -> Callable
+
+    if not (
+        inspect.isclass(permission_class)
+        and issubclass(permission_class, BasePermission)
+    ):
+        # In case the decorator's called without parenthesis (`@api_view`),
+        # the `permission_class` variable holds the real view.
+        return api_view()(permission_class)
+
+    deserializers_http_methods_map = build_deserializer_map(deserializer_class)
+
+    def view_decorator(view):
+        # type:(Union[Callable, ClassVar]) -> Union[Callable, ClassVar]
+        if inspect.isfunction(view):
+            return build_function_wrapper(
+                permission_class,
+                allowed_methods,
+                deserializers_http_methods_map,
+                allow_forms,
+                view,
+            )
+        elif inspect.isclass(view) and issubclass(view, View):
+            return build_class_wrapper(
+                permission_class,
+                allowed_methods,
+                deserializers_http_methods_map,
+                allow_forms,
+                view,
+            )
+        given_str = (
+            "{} class".format(view.__name__)
+            if inspect.isclass(view)
+            else "{} object".format(view.__class__.__name__)
+        )
+        raise TypeError(
+            "The `@api_view` decorator is applied to either a function or a "
+            "class inheriting from django.views.View. Given: {}".format(given_str)
+        )
+
+    return view_decorator
